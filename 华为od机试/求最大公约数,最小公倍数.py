46b980a8271879bc71355a33b1b746b162b4ef6d
heracleo <alialialili@protonmail.com>
2022-11-05 09:42:26 +0800

    add blank line

--- 华为od机试/求最大公约数,最小公倍数.py
+++ 华为od机试/求最大公约数,最小公倍数.py
@@ -4,10 +4,12 @@ def gcd(x, y):
         return max(x,y)
     return gcd(min(x,y), max(x,y)%min(x,y))
 
+
 # 最小公倍数为 两数乘积除以最大公约数
 def lcm(x, y):
     n = gcd(x,y)
     return int(x*y/n)
+
 
 a, b = 369, 289
 print(gcd(a, b))
